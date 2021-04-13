e90e193a0e63f0a367e7c92d43cf77f918422fe2
Kamil Put <kamil.put@interia.pl>
2021-04-13 15:31:56 +0200

    Add class 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -31,14 +31,20 @@ const titleClickHandler = function(event){
         activeArticle.classList.remove('active');
     }
 
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
 
     const articleSelector = clickedElement.getAttribute('href');
     console.log(articleSelector);
 
-    /* find the correct article using the selector (value of 'href' attribute) */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
 
-    /* add class 'active' to the correct article */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+
+    /* [DONE] add class 'active' to the correct article */
+
+    targetArticle.classList.add('active');
+    console.log('targetArticle:', targetArticle);
 }
   
 const links = document.querySelectorAll('.titles a');
