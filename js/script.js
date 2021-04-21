4ea8df0dd4c018929a1e389866190d7b11275ffc
Kamil Put <kamil.put@interia.pl>
2021-04-21 11:37:26 +0200

    Add functions and styles for sidebar tags

--- js/script.js
+++ js/script.js
@@ -47,7 +47,10 @@
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles',
     optArticleTagsSelector = '.post-tags .list',
-    optArticleAuthorSelector = '.post-author';
+    optArticleAuthorSelector = '.post-author',
+    optTagsListSelector = '.tags.list',
+    optCloudClassCount = '3',
+    optCloudClassPrefix = 'tag-size-';
 
   function generateTitleLinks(customSelector = ''){
 
@@ -94,7 +97,31 @@
 
   generateTitleLinks();
 
+  function calculateTagsParams(tags){
+    const params = {max: '0', min: '999999'};
+    for(let tag in tags){
+      console.log(tag + ' is used ' + tags[tag] + ' times');
+      if(tags[tag] > params.max){
+        params.max = tags[tag];
+      }
+      if(tags[tag] < params.min) {
+        params.min = tags[tag];
+      }
+    }
+    return params;
+  }
+
+  function calculateTagClass(count, params){
+    const classNumber = Math.floor( ( (count - params.min) / (params.max - params.min) ) * (optCloudClassCount - 1) + 1 );
+
+    return optCloudClassPrefix + classNumber;
+  }
+
   function generateTags(){
+    /* [NEW] create a new variable allTags with an empty object */
+
+    let allTags = {};
+
     /* find all articles */
 
     const articles = document.querySelectorAll(optArticleSelector);
@@ -130,12 +157,48 @@
         /* add generated code to html variable */
 
         html = html + linkHTML;
+
+        /* [NEW] check if this link is NOT already in allTags */
+
+        if(!allTags[tag]) {
+          /* [NEW] add tag to allTags object */
+          allTags[tag] = 1;
+        } else {
+          allTags[tag]++;
+        }
       }
 
       /* insert HTML of all the links into the tags wrapper */
 
       tagsWrapper.innerHTML = html;
     }
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector('.tags');
+
+    const tagsParams = calculateTagsParams(allTags);
+    console.log('tagsParams:', tagsParams);
+
+    /* [NEW] create variable for all links HTML code */
+    let allTagsHTML = '';
+
+    /* [NEW] START LOOP: for each tag in allTags: */
+    for(let tag in allTags){
+      /* [NEW] generate code of a link and add it to allTagsHTML */
+      //allTagsHTML +=  '<li><a href="#tag-' + tag + '">' + tag + '</a> (' + allTags[tag] +')' +'</li> ';
+
+      //const tagLinkHTML = '<li><a class="' + calculateTagClass(allTags[tag], tagsParams) + '" href="#tag-' + tag + '">' + tag + '</a> (' + allTags[tag] +')' +'</li> ';
+
+      const tagLinkHTML = '<a class="' + calculateTagClass(allTags[tag], tagsParams) + '" href="#tag-' + tag + '">' + tag + '</a>';
+      console.log('tagLinkHTML:', tagLinkHTML);
+      allTagsHTML += tagLinkHTML;
+      //console.log(allTagsHTML);
+    }
+    /* [NEW] END LOOP: for each tag in allTags: */
+
+    /*[NEW] add HTML from allTagsHTML to tagList */
+
+    tagList.innerHTML = allTagsHTML;
+    //console.log(allTagsHTML);
   }
 
   generateTags();
@@ -159,6 +222,7 @@
     }
     /* find all tag links with "href" attribute equal to the "href" constant */
     const tagLinks = document.querySelectorAll('a[href="' + href + '"]');
+
     /* START LOOP: for each found tag link */
     for(let tagLink of tagLinks){
       /* add class active */
